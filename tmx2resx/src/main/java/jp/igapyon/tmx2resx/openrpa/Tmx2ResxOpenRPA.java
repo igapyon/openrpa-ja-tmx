5a1f315a46ea977f15197803dbf35e37d2f2c8b8
Toshiki Iga <>
2020-08-23 17:18:09 +0900

    save

--- tmx2resx/src/main/java/jp/igapyon/tmx2resx/openrpa/Tmx2ResxOpenRPA.java
+++ tmx2resx/src/main/java/jp/igapyon/tmx2resx/openrpa/Tmx2ResxOpenRPA.java
@@ -18,6 +18,18 @@ package jp.igapyon.tmx2resx.openrpa;
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import jp.igapyon.tmx2resx.Tmx2Resx;
+
 public class Tmx2ResxOpenRPA {
     public static void main(String[] args) throws IOException {
         File fileRoot = new File("../../openrpa/");
@@ -27,6 +39,27 @@ public class Tmx2ResxOpenRPA {
         File file = new File(fileRoot, "OpenRPA/Resources/strings.resx");
         if (!file.exists()) {
             throw new IOException("resx filenot found: " + file.getAbsolutePath());
+        }
+
+        String resxString = FileUtils.readFileToString(file, "UTF-8");
+        resxString = resxString.replace("\uFEFF", ""); // BOM
+
+        System.err.println(resxString);
+
+        Document document = Tmx2Resx.string2dom(resxString);
+
+        final XPath xpath = XPathFactory.newInstance().newXPath();
+        try {
+            final NodeList list = (NodeList) xpath.evaluate("/root/data", document, XPathConstants.NODESET);
+            for (int index = 0; index < list.getLength(); index++) {
+                System.err.println(list.item(index));
+
+                final Node nodeValue = (Node) xpath.evaluate("value", list.item(index), XPathConstants.NODE);
+                System.err.println(nodeValue.getTextContent());
+            }
+        } catch (XPathExpressionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
